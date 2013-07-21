ab020f0383e6c563154cf27a3e070c0763308f16
Rizwan Javaid <rizwan@rizwanjavaid.com>
2013-07-20 19:47:28 -0700

    This is a test

---
+++ Journey/WelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  WelcomeViewController.h
+//  Journey
+//
+//  Created by Rizwan Javaid-Old on 7/20/13.
+//  Copyright (c) 2013 Rizwan Javaid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WelcomeViewController : UIViewController
+
+@end
---
+++ Journey/WelcomeViewController.m
@@ -0,0 +1,52 @@
+//
+//  WelcomeViewController.m
+//  Journey
+//
+//  Created by Rizwan Javaid-Old on 7/20/13.
+//  Copyright (c) 2013 Rizwan Javaid. All rights reserved.
+//
+
+#import "WelcomeViewController.h"
+
+@interface WelcomeViewController ()
+
+@end
+
+@implementation WelcomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [self.navigationController setNavigationBarHidden:YES animated:animated];
+    [super viewWillAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [self.navigationController setNavigationBarHidden:NO animated:animated];
+    [super viewWillDisappear:animated];
+}
+
+@end
